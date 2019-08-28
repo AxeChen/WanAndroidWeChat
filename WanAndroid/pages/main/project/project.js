e55096a51a28b7e5df874b5c79b1a2d88008c23b
MiaoLee <1727322019@qq.com>
2019-08-28 11:23:39 +0800

    1

--- WanAndroid/pages/main/project/project.js
+++ WanAndroid/pages/main/project/project.js
@@ -1,20 +1,32 @@
-// pages/main/project/project.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    test: "ok",
+    newlist: [],
+    num: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getTest()
   },
-
+  getTest() {
+    let that = this
+    wx.request({
+      url: "https://wanandroid.com/article/listproject/0/json",
+      success: (data => {
+        that.setData({
+          newlist: data.data.data.datas
+        })
+        wx.stopPullDownRefresh();
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -47,14 +59,25 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.getTest()
   },
-
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    let that = this
+    var numonfun = that.data.num + 1
+    wx.request({
+      url: "https://wanandroid.com/article/listproject/" + numonfun + "/json",
+      success: (data => {
+        var newarr = data.data.data.datas
+        var list = that.data.newlist
+        that.setData({
+          newlist: list.concat(newarr),
+          num: numonfun
+        })
+      })
+    })
   },
 
   /**
@@ -62,5 +85,10 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  primary: function () {
+    this.setData({
+      test: "new"
+    })
   }
 })
